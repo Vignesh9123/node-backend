95d50dcd4ce96a75c36fa3024ad290df4ca3ea29
Vignesh D <75655439+Vignesh9123@users.noreply.github.com>
2024-03-28 23:28:08 +0530

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,7 +5,6 @@ const bodyparser = require("body-parser")
 app.use(bodyparser.json())
 app.post("/signup",async(req,res)=>{
     let body = req.body
-    res.send("Hello")
     const {email,username,password} = body
     let userExists = await Users.findOne({email})
     if(userExists){
