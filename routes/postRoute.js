f449a2a1dade35a69bcd3d9512e10853eeecb33d
Vignesh D <75655439+Vignesh9123@users.noreply.github.com>
2024-03-29 22:25:58 +0530

    updated postRoute

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,5 +1,6 @@
 const { default: getPosts } = require("../controllers/postController")
 const app = require("express").Router()
+const {getPostbyId} = require("../controllers/postController")
 
 app.get("/",getPosts)
 app.get("/:id", getPostbyId)
